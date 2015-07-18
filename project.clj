799e91937c36dee6571e25b20a3e3226d9ff5029
crocket <crockabiscuit@gmail.com>
2015-07-18 23:25:38 +0900

    Upgrade clojure to 1.7.0

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject clj-ddns-client "1.2.0-SNAPSHOT"
+(defproject clj-ddns-client "1.2.0"
   :description "It updates DDNS entries of various DDNS providers."
   :url "http://github.com/crocket/clj-ddns-client"
   :license {:name "GNU Lesser General Public License v3.0"
             :url "http://www.gnu.org/licenses/lgpl-3.0.txt"
             :year 2015
             :key "lgpl-3.0"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [clj-time "0.10.0"]
                  [jarohen/chime "0.1.6"]
                  [clj-http "1.1.2"]
